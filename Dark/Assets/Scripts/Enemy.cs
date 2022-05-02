99c9e7314602478df595c784d1a91f867669ee01
Nejjer <artemijv65@gmail.com>
2022-05-02 17:18:13 +0500

    add light(not work)

--- Dark/Assets/Scripts/Enemy.cs
+++ Dark/Assets/Scripts/Enemy.cs
@@ -8,12 +8,12 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject Loot;
     [SerializeField] private GameObject Fuel;
-    private int _health = 100;
+    [SerializeField] private int health = 100;
     public void GetDamage(int damage)
     {
-        _health -= damage;
-        Debug.Log(_health);
-        if (_health <= 0)
+        health -= damage;
+        Debug.Log(health);
+        if (health <= 0)
         {
             Instantiate(Random.Range(0, 1) > 0.5 ? Loot : Fuel, transform.position, Quaternion.identity);
             Destroy(gameObject);
