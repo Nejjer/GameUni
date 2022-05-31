acf753e7c23a2173c374a9246367ff007428c382
Nejjer <artemijv65@gmail.com>
2022-05-31 20:01:00 +0500

    release

--- Dark/Assets/Scripts/Light/fixedBUDKAUI.cs
+++ Dark/Assets/Scripts/Light/fixedBUDKAUI.cs
@@ -5,20 +5,19 @@ public class fixedBUDKAUI : MonoBehaviour
 {
     [SerializeField] private Text text;
     [SerializeField] private GameObject textContainer;
-    [SerializeField] private Vector3 offset;
-    private Camera _camera;
-
-    private void Start() => _camera = Camera.main;
 
     private void Update()
     {
-        textContainer.transform.position = _camera.WorldToScreenPoint(transform.position + offset);
+        textContainer.transform.position = transform.position;
     }
 
     public void SetProgress(int currentItems, int requiredItems)
     {
         if (currentItems == requiredItems)
+        {
             textContainer.SetActive(false);
+            GetComponent<AudioSource>().Play();
+        }
         text.text = $"{currentItems}/{requiredItems}";
     }
 }
